64534f5685df1e43c8d9da8ac0ea79457937cf02
Zoya M <zkulesza27@gmail.com>
2019-06-10 18:57:51 +0200

    Add files via upload

--- HibernateSampleExample/src/main/java/com/javawebtutor/service/Manager.java
+++ HibernateSampleExample/src/main/java/com/javawebtutor/service/Manager.java
@@ -1,5 +1,24 @@
 package com.javawebtutor.service;
 
-public class Manager {
+import org.hibernate.SessionFactory;
 
+import com.javawebtutor.model.HibernateUtil;
+import com.javawebtutor.model.Tytul;
+
+public class Manager {
+	SessionFactory sessionFactory;
+	
+	public Manager() {
+		sessionFactory = HibernateUtil.getSessionFactory();
+	}
+	
+	public void addTytul(Tytul tytul) {
+		tytul.setid_tytul(null);
+		sessionFactory.getCurrentSession().persist(tytul);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Tytul> getAllTytul() {
+		return sessionFactory.getCurrentSession().getNamedQuery("tytul.all").list();
+	}
 }
