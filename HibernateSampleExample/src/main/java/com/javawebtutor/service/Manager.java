c2ec3a89cef29e383c4f4bcc5879e52095f29bac
Zoya M <zkulesza27@gmail.com>
2019-06-10 19:11:41 +0200

    Add files via upload

--- HibernateSampleExample/src/main/java/com/javawebtutor/service/Manager.java
+++ HibernateSampleExample/src/main/java/com/javawebtutor/service/Manager.java
@@ -1,8 +1,12 @@
 package com.javawebtutor.service;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 
 import com.javawebtutor.model.HibernateUtil;
+import com.javawebtutor.model.KlientPubliczny;
+import com.javawebtutor.model.Pracownik;
 import com.javawebtutor.model.Tytul;
 
 public class Manager {
@@ -21,4 +25,26 @@ public class Manager {
 	public List<Tytul> getAllTytul() {
 		return sessionFactory.getCurrentSession().getNamedQuery("tytul.all").list();
 	}
+	
+	public void addPracownik(Pracownik pracownik) {
+		pracownik.setid_pracownik(null);
+		sessionFactory.getCurrentSession().persist(pracownik);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Pracownik> getAllPracownik() {
+		return sessionFactory.getCurrentSession().getNamedQuery("pracownik.all").list();
+	}
+	
+	public void addPracownik(KlientPubliczny klientPubliczny) {
+		klientPubliczny.setid_klientPubliczny(null);
+		sessionFactory.getCurrentSession().persist(klientPubliczny);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<KlientPubliczny> getAllKlientPuliczny() {
+		return sessionFactory.getCurrentSession().getNamedQuery("klientPubliczny.all").list();
+	}
+	
+		
 }
